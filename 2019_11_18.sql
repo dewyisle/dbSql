538a4e7f8afa7b39169f647dab1926fb1c66f4e3
dew <dewyisle@gamil.com>
2019-11-19 17:11:46 +0900

    idx3 문제 확인 synonym 권한(DCL)  . 시스템 권한  . 객체 권한
    GRANT, REVOKE
    DATA DICTIONARY
    
    전국 도시 발전지수 검증

---
+++ 2019_11_18.sql
@@ -0,0 +1,64 @@
+---------------------------------------------hr계정
+SELECT *
+FROM USER_VIEWS;
+
+SELECT *
+FROM ALL_VIEWS
+WHERE OWNER = 'PC01';
+
+SELECT *
+FROM pc01.V_EMP_DEPT;
+
+--pc01 계정에서 조회권한을 받은 V_EMP_DEPT view를 hr 계정에서 조회하기 위해서는
+--계정명.view이름 형식으로 기술을 해야한다.
+--매번 계정명을 기술하기 귀찮으므로 시노님을 통해 다른 별칭을 생성
+
+CREATE SYNONYM V_EMP_DEPT FOR pc01.V_EMP_DEPT;
+
+--pc01.V_EMP_DEPT --> V_EMP_DEPT
+SELECT *
+FROM V_EMP_DEPT;
+
+--시노님 삭제
+DROP SYNONYM V_EMP_DEPT;
+
+--hr계정 비밀번호 : java
+--hr계정 비밀번호 변경: hr
+ALTER USER hr IDENTIFIED BY hr;
+--ALTER USER pc01 IDENTIFIED by java; --본인 계정이 아니라 에러
+
+
+
+--dictionary
+--접두어 : USER : 사용자 소유 객체
+--         ALL : 사용자가 사용가능한 객체
+--         DBA : 관리자 관점의 전체 객체 (일반 사용자는 사용 불가)
+--         V$ : 시스템과 관련된 view (일반 사용자는 사용 불가)
+
+SELECT *
+FROM USER_TABLES;
+
+SELECT *
+FROM ALL_TABLES;
+
+SELECT *
+FROM DBA_TABLES
+WHERE OWNER IN('PC01', 'HR');
+
+--오라클에서 동일한 SQL이란?
+--문자가 하나라도 틀리면 안됨
+-- 다음 sql들은 같은 결과를 만들어 낼지 몰라도 DBMS에서는 
+-- 서로 다른 SQL로 인식된다.
+SELECT /*bind_test*/* FROM emp;
+Select /*bind_test*/* FROM emp;
+Select /*bind_test*/*  FROM emp;
+
+Select /*bind_test*/*  FROM emp WHERE empno=7369;
+Select /*bind_test*/*  FROM emp WHERE empno=7499;
+Select /*bind_test*/*  FROM emp WHERE empno=7521;
+
+Select /*bind_test*/*  FROM emp WHERE empno=:empno;
+
+SELECT *
+FROM v$SQL;
+
